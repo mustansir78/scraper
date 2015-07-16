a8d20db6e1e7ff262d849ecd82b087daf8b24598
mustansir78 <mustansir.husain@gmail.com>
2015-07-16 12:16:00 +0530

    First commit to 0.2

---
+++ consolePOC/PYLLRecord.cs
@@ -0,0 +1,19 @@
+﻿
+namespace consolePOC
+{
+    public class PYLLRecord
+    {
+        public string Reporting_Period { get; set; }
+        public string Breakdown { get; set; }
+        public string Level { get; set; }
+        public string Level_Description { get; set; }
+        public string Condition { get; set; }
+        public string Gender { get; set; }
+        public string DSR { get; set; }
+        public string CI_Lower { get; set; }
+        public string CI_Upper { get; set; }
+        public string Registered_Patients { get; set; }
+        public string Years_of_life_lost { get; set; }
+        public string Observed_deaths { get; set; }
+    }
+}
